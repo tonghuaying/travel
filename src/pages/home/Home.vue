fcda3b2ce6aff309483250eb3a50bd8fc7b5b212
tong <906650474@qq.com>
2020-02-26 21:15:39 +0800

    init project

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+    <div class="home">hello world</div>
+    <!-- <router-link to="/list" class="home">列表页</router-link> -->
+</div>
+
+</template>
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+<style scoped>
+
+</style>
