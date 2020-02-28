748276f060f340a75b3c30004f3fe00a80c3b74c
tong <906650474@qq.com>
2020-02-28 16:28:15 +0800

    city list

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,33 @@
+<template>
+<ul class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+</ul>
+</template>
+<script>
+
+export default {
+  name: 'CityAlphabet'
+
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.list
+   position: absolute
+   right: 0
+   top: 1.58rem
+   bottom: 0;
+   width: .4rem
+   display: flex
+   flex-direction: column
+   justify-content: center
+   .item
+      line-height: .44rem
+      text-align: center
+      background: $bgcolor
+      color: #fff
+</style>
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="list" ref="wrapper">
+        <div>
+            <div class="area">
+                <div class="title border-topbottom">当前城市</div>
+                <div class="button-list">
+                    <div class="button">深圳</div>
+                    <div class="button">深圳</div>
+                    <div class="button">深圳</div>
+                    <div class="button">深圳</div>
+                </div>
+            </div>
+            <div class="area">
+                <div class="title border-topbottom">热门城市</div>
+                <div class="button-list">
+                    <div class="button">深圳</div>
+                    <div class="button">深圳</div>
+                    <div class="button">深圳</div>
+                    <div class="button">深圳</div>
+                </div>
+            </div>
+            <div class="area">
+                <div class="title border-topbottom">A</div>
+                <ul class="area-list">
+                    <li class="area-list-item">city</li>
+                    <li class="area-list-item">city</li>
+                    <li class="area-list-item">city</li>
+                    <li class="area-list-item">city</li>
+                    <li class="area-list-item">city</li>
+                    <li class="area-list-item">city</li>
+                    <li class="area-list-item">city</li>
+                    <li class="area-list-item">city</li>
+                    <li class="area-list-item">city</li>
+                    <li class="area-list-item">city</li>
+                    <li class="area-list-item">city</li>
+                    <li class="area-list-item">city</li>
+                    <li class="area-list-item">city</li>
+                    <li class="area-list-item">city</li>
+                    <li class="area-list-item">city</li>
+                    <li class="area-list-item">city</li>
+                    <li class="area-list-item">city</li>
+                    <li class="area-list-item">city</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import BScroll from 'better-scroll'
+export default {
+  name: 'CityList',
+  mounted () {
+    console.log(this.$refs.wrapper)
+    this.scroll = new BScroll(this.$refs.wrapper)
+  }
+
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.border-topbottom
+  &:before
+    border-color: #ccc
+  &:after
+    border-color: #ccc
+.title
+  line-height: .4rem
+  background: #eee
+  padding: .1rem .2rem
+  color: #666
+  font-size: .26rem
+.button-list
+   padding: .1rem
+   display: flex
+   width: 100%
+   flex-wrap: wrap
+   .button
+       width: 25%
+       margin: .1rem
+       border: solid 1px #eee
+       padding: .1rem
+       border-radius: .03rem
+       text-align: center
+.area-list
+    margin-bottom: .2rem
+    .area-list-item
+       padding: .2rem .15rem
+       border-bottom: solid 1px #dcdcdc
+
+</style>
